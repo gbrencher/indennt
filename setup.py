f138d93c96e18e95ec26a4263e948d9a5b6cc900
gbrencher <gbrench@uw.edu>
2024-09-03 13:45:38 -0700

    updating for package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import setuptools
+
+setup(name='indennt',
+      version='0.1',
+      description='Interferogram denoising convolutional neural network',
+      packages=setuptools.find_packages(),
+     )
